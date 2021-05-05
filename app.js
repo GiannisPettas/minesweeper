c66494852dc5a1da65bcb13da01da8c2d9809b51
CORYOLL <pettasg@yahoo.gr>
2021-05-05 22:42:27 +0300

    added bombs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid');
-const rows = 10;
+const rows = 13;
 const columns = 10;
 const numOfBombs = 10;
 const tilesObj = {};
@@ -8,7 +8,11 @@ const tilesObj = {};
 for (let i = 1; i <= rows; i++) {
   for (let n = 1; n <= columns; n++) {
     const tileDiv = document.createElement('div');
-    const tileID = `${('0' + i).slice(-2)}${('0' + n).slice(-2)}`;
+    const tileID = joinTwoNumbersWithPadding(n, i);
+    tileDiv.x = n;
+    tileDiv.y = i;
+    tileDiv.tileID = tileID;
+    tileDiv.bomb = false;
 
     tileDiv.setAttribute('position', tileID);
     tileDiv.setAttribute('state', 'closed');
@@ -20,7 +24,21 @@ for (let i = 1; i <= rows; i++) {
 }
 
 //add bombs
+for (let i = numOfBombs; i > 0; ) {
+  let bombX = Math.floor(Math.random() * columns) + 1;
+  let bombY = Math.floor(Math.random() * rows) + 1;
+  let tileWithBomb = joinTwoNumbersWithPadding(bombX, bombY);
+  tilesObj[tileWithBomb].bomb = true;
+  tilesObj[tileWithBomb].setAttribute('bomb', 'true');
+  i--;
+}
 
 function changeTileState(e) {
-  e.target.setAttribute('state', 'open');
+  const clickedTile = e.target;
+  clickedTile.setAttribute('state', 'open');
+  console.log(`${clickedTile.x},${clickedTile.y},${clickedTile.tileID}`);
+}
+
+function joinTwoNumbersWithPadding(num1, num2, padding = 2) {
+  return `${('0' + num1).slice(-2)}${('0' + num2).slice(-2)}`;
 }
