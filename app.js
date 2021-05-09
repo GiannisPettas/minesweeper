7e5ab31a97a1c2ad3a38a162851ea197ae33f887
CORYOLL <pettasg@yahoo.gr>
2021-05-09 14:12:54 +0300

    created number indicators for bombs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector('.grid');
 const rows = 13;
 const columns = 10;
-const numOfBombs = 10;
+const numOfBombs = 20;
 const tilesObj = {};
 
 // initalize grid
@@ -13,6 +13,7 @@ for (let i = 1; i <= rows; i++) {
     tileDiv.y = i;
     tileDiv.tileID = tileID;
     tileDiv.bomb = false;
+    tileDiv.bombDetectNum = 0;
 
     tileDiv.setAttribute('position', tileID);
     tileDiv.setAttribute('state', 'closed');
@@ -28,17 +29,46 @@ for (let i = numOfBombs; i > 0; ) {
   let bombX = Math.floor(Math.random() * columns) + 1;
   let bombY = Math.floor(Math.random() * rows) + 1;
   let tileWithBomb = joinTwoNumbersWithPadding(bombX, bombY);
-  tilesObj[tileWithBomb].bomb = true;
-  tilesObj[tileWithBomb].setAttribute('bomb', 'true');
-  i--;
+  let tileWithBombDiv = tilesObj[tileWithBomb];
+  if (!tileWithBombDiv.bomb) {
+    tileWithBombDiv.bomb = true;
+    tileWithBombDiv.setAttribute('bomb', 'true');
+    tileWithBombDiv.bombDetectNum = 'B';
+    tileWithBombDiv.innerText = tileWithBombDiv.bombDetectNum;
+
+    const arrayWithTilesAroundTile = findAllTilesAroundTile(tileWithBombDiv);
+    for (let i = 0; i < arrayWithTilesAroundTile.length; i++) {
+      if (arrayWithTilesAroundTile[i].bomb !== true) {
+        arrayWithTilesAroundTile[i].bombDetectNum += 1;
+        arrayWithTilesAroundTile[i].innerText =
+          arrayWithTilesAroundTile[i].bombDetectNum;
+      }
+    }
+    i--;
+  }
 }
 
 function changeTileState(e) {
   const clickedTile = e.target;
   clickedTile.setAttribute('state', 'open');
-  console.log(`${clickedTile.x},${clickedTile.y},${clickedTile.tileID}`);
 }
 
 function joinTwoNumbersWithPadding(num1, num2, padding = 2) {
   return `${('0' + num1).slice(-2)}${('0' + num2).slice(-2)}`;
+}
+
+function findAllTilesAroundTile(tile) {
+  const arrayWithTilesAroundTile = [];
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      if (!(x === 0 && y === 0)) {
+        const tileAroundID = joinTwoNumbersWithPadding(tile.x + x, tile.y + y);
+        const tileAround = tilesObj[tileAroundID];
+        if (tileAround) {
+          arrayWithTilesAroundTile.push(tileAround);
+        }
+      }
+    }
+  }
+  return arrayWithTilesAroundTile;
 }
